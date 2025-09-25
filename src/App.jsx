d132092708728f5417b31834e1903d50a8e5e543
Hazakura2024 <ytr240.jp@gmail.com>
2025-09-25 16:52:02 +0900

    todoに入力された文章を管理するステートを定義

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,34 @@ import { useState } from 'react'
 import './App.css'
 
 export const App = () => {
+  
+  //入力されているtodoの文章を管理するステート
+  const {todoText , setTodoText} = useState("todoTextの初期値");
 
 
   return (
     <>
-      <h1>文字</h1>
+      <h1>Todoアプリ</h1>
       <div className='input-area'>
-        <input type="text" />
+        <input type="text" placeholder='TODO入力' value={todoText} />
         <button>追加</button>
       </div>
-      <div>
+      <div className='incomplete-area'>
         <ul>
           <li>
-            <div>
-              <p>todo1</p>
+            <div className='todo-low'>
+              <p className='todo-item'>todo1</p>
               <button>完了</button>
               <button>削除</button>
             </div>
           </li>
         </ul>
       </div>
-      <div>
+      <div className='complete-area'>
         <ul>
           <li>
-            <div>
-              <p>todo2</p>
+            <div className='todo-low'>
+              <p className='todo-item'>todo2</p>
               <button>戻す</button>
             </div>
           </li>
