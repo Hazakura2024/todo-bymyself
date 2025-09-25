34d5960d82cd74a9a33905ac587d1325df85fefa
Hazakura2024 <ytr240.jp@gmail.com>
2025-09-25 17:54:57 +0900

    完了ボタンまで作成

--- src/App.jsx
+++ src/App.jsx
@@ -21,57 +21,107 @@ export const App = () => {
 
   //追加ボタンを押したときに呼び出される関数
   //incompleteTodosに入力されたtodoを追加する
-  const addIncompleteTodo = () => {
+  const onCkickAddIncompleteTodo = () => {
 
     //何も入力されてないときに空白のtodoが追加されるのを防止
     if (todoText === "") {
       return;
     }
 
+    //incompleteTodo配列に追加
     const newtodos = [...incompleteTodos, { id: Date.now(), text: todoText }]
     setIncompleteTodos(newtodos)
+
+    //入力欄をクリア
+    setTodoText("")
 
   }
 
 
+  //削除ボタンを押したときに呼び出される関数
+  //incompleteTodosからボタンを押されたtodoを削除する
+  const onClickDeleteIncompleteTodo = (index) => {
+
+    // alert();
+
+    const newtodos = [...incompleteTodos]
+
+    newtodos.splice(index, 1)
+
+    setIncompleteTodos(newtodos)
+  }
+
+
+  //完了ボタンを押したときに呼び出される関数
+  //completeTodosにボタンを押されたtodoを追加、incompleteTodosからボタンを押されたtodoを削除する
+  const onClickCompleteIncompleteTodo = (index) => {
+    // alert()
+
+    const newincompletetodos = [...incompleteTodos]
+    const newcompletetodos = [...completeTodos, incompleteTodos[index]]
+
+    newincompletetodos.splice(index, 1)
+
+    setIncompleteTodos(newincompletetodos)
+    setCompleteTodos(newcompletetodos)
+
+
+  }
+
+
+  //戻すボタンが押したときに呼び出される関数
+  //completeTodosにボタンを押されたtodoを追加、incompleteTodosからボタンを押されたtodoを削除する
+
+
   return (
     <>
-      <h1>Todoアプリ</h1>
-      <div className='input-area'>
-        <input type="text" placeholder='TODO入力' value={todoText} onChange={onChangeTodoText} />
-        <button onClick={addIncompleteTodo}>追加</button>
-      </div>
-      <div className='incomplete-area'>
-        <p className='title'>未完了のタスク</p>
-        <ul>
-          <li>
+      <div className='todo-app'>
+        <h1 className='main-title'>Todoアプリ</h1>
+        <div className='input-area'>
+          <input type="text" placeholder='TODO入力' value={todoText} onChange={onChangeTodoText} />
+          <button onClick={onCkickAddIncompleteTodo}>追加</button>
+        </div>
+        <div className='incomplete-area'>
+          <p className='title'>未完了のタスク</p>
+          <ul>
+            {/* <li>
             <div className='todo-low'>
               <p className='todo-item'>todo1</p>
               <button>完了</button>
               <button>削除</button>
             </div>
-          </li>
-          {incompleteTodos.map((todo,index) => (
-            <li key={todo.id}>
-              <div className='todo-low'>
-                <p className='todo-item'>{todo.text}</p>
-                <button>完了</button>
-                <button>削除</button>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className='complete-area'>
-        <p className='title'>完了済のタスク</p>
-        <ul>
-          <li>
+          </li> */}
+            {incompleteTodos.map((todo, index) => (
+              <li key={todo.id}>
+                <div className='todo-low'>
+                  <p className='todo-item'>{todo.text}</p>
+                  <button onClick={onClickCompleteIncompleteTodo.bind(null, index)}>完了</button>
+                  <button onClick={onClickDeleteIncompleteTodo.bind(null, index)}>削除</button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className='complete-area'>
+          <p className='title'>完了済のタスク</p>
+          <ul>
+            {/* <li>
             <div className='todo-low'>
               <p className='todo-item'>todo2</p>
               <button>戻す</button>
             </div>
-          </li>
-        </ul>
+          </li> */}
+            {completeTodos.map((todo, index) => (
+              <li key={todo.id}>
+                <div className='todo-low'>
+                  <p className='todo-item'>{todo.text}</p>
+                  <button>戻す</button>
+                </div>
+              </li>
+
+            ))}
+          </ul>
+        </div>
       </div>
     </>
   )
