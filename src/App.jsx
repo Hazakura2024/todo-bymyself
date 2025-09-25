58c0e169b9165694b020ec722b118f45eda22490
Hazakura2024 <ytr240.jp@gmail.com>
2025-09-25 17:24:33 +0900

    todo追加機能を作成

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,27 @@ import { useState } from 'react'
 import './App.css'
 
 export const App = () => {
-  
+
   //入力されているtodoの文章を管理するステート
-  const [todoText , setTodoText] = useState("");
+  const [todoText, setTodoText] = useState("");
 
   //未完了のtodoを管理するステート
-  const [incompleteTodos ,setIncompleteTodos] = useState()
+  const [incompleteTodos, setIncompleteTodos] = useState([])
 
   //todoの入力欄の文字が変更されたときに呼び出される関数
   //todoに入力された値を変更する
   const onChangeTodoText = (event) => {
     setTodoText(event.target.value);
-    
+
   }
 
   //追加ボタンを押したときに呼び出される関数
   //incompleteTodosに入力されたtodoを追加する
   const addIncompleteTodo = () => {
+    
 
-    const newtodos = [...incompleteTodos,{id: Date.now(),text: todoText}]
-    setTodoText(newtodos)
+    const newtodos = [...incompleteTodos, { id: Date.now(), text: todoText }]
+    setIncompleteTodos(newtodos)
 
   }
 
@@ -30,7 +31,7 @@ export const App = () => {
     <>
       <h1>Todoアプリ</h1>
       <div className='input-area'>
-        <input type="text" placeholder='TODO入力' value={todoText} onChange={onChangeTodoText}/>
+        <input type="text" placeholder='TODO入力' value={todoText} onChange={onChangeTodoText} />
         <button onClick={addIncompleteTodo}>追加</button>
       </div>
       <div className='incomplete-area'>
@@ -43,6 +44,15 @@ export const App = () => {
               <button>削除</button>
             </div>
           </li>
+          {incompleteTodos.map((todo,index) => (
+            <li key={todo.id}>
+              <div className='todo-low'>
+                <p className='todo-item'>{todo.text}</p>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
       <div className='complete-area'>
