9867e4061a46036ad514c630bca818972349763f
Hazakura2024 <ytr240.jp@gmail.com>
2025-09-25 16:43:45 +0900

    ひな形を作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+
 
   return (
     <>
+      <h1>文字</h1>
+      <div className='input-area'>
+        <input type="text" />
+        <button>追加</button>
+      </div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <ul>
+          <li>
+            <div>
+              <p>todo1</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <ul>
+          <li>
+            <div>
+              <p>todo2</p>
+              <button>戻す</button>
+            </div>
+          </li>
+        </ul>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
-export default App
