fddf55ab6ca98b7ffff9bdcc70d7780c41cd5c4a
Hazakura2024 <ytr240.jp@gmail.com>
2025-09-26 13:30:12 +0900

    viteでのpages公開を設定

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react({
@@ -10,4 +10,8 @@ export default defineConfig({
       },
     }),
   ],
+  base: '/todo-bymyself/',
+  build: {
+    outDir: 'dist'
+  }
 })
