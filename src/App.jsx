cf01f92e74d6069f189c78bb4c83610cfa6354cc
西尾優太朗 <ytr240.jp@gmail.com>
2025-09-26 16:27:08 +0900

    Revert "Component"

--- src/App.jsx
+++ src/App.jsx
@@ -1,106 +1,35 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import { InputTodo } from './conponent/InputTodo';
-import { IncompleteTodo } from './conponent/IncompleteTodo';
-import { CompleteTodo } from './conponent/CompleteTodo';
 
-export const App = () => {
-
-  //入力されているtodoの文章を管理するステート
-  const [todoText, setTodoText] = useState("");
-
-  //未完了のtodoを管理するステート
-  const [incompleteTodos, setIncompleteTodos] = useState([])
-
-  //完了済のtodoを管理するステート
-  const [completeTodos, setCompleteTodos] = useState([])
-
-  //todoの入力欄の文字が変更されたときに呼び出される関数
-  //todoに入力された値を変更する
-  const onChangeTodoText = (event) => {
-    setTodoText(event.target.value);
-
-  }
-
-  //追加ボタンを押したときに呼び出される関数
-  //incompleteTodosに入力されたtodoを追加する
-  //余談、命名規則を統一したい
-  const onCkickAddIncompleteTodo = () => {
-
-    //何も入力されてないときに空白のtodoが追加されるのを防止
-    if (todoText === "") {
-      return;
-    }
-
-    //incompleteTodo配列に追加
-    //todoのidには時刻を指定
-    const newtodos = [...incompleteTodos, { id: Date.now(), text: todoText }]
-    setIncompleteTodos(newtodos)
-
-    //入力欄をクリア
-    setTodoText("")
-
-  }
-
-
-  //削除ボタンを押したときに呼び出される関数
-  //incompleteTodosからボタンを押されたtodoを削除する
-  const onClickDeleteIncompleteTodo = (index) => {
-
-    // alert();
-
-    const newtodos = [...incompleteTodos]
-
-    newtodos.splice(index, 1)
-
-    setIncompleteTodos(newtodos)
-  }
-
-
-  //完了ボタンを押したときに呼び出される関数
-  //completeTodosにボタンを押されたtodoを追加、incompleteTodosからボタンを押されたtodoを削除する
-  const onClickCompleteIncompleteTodo = (index) => {
-    // alert()
-
-    const newincompletetodos = [...incompleteTodos]
-    const newcompletetodos = [...completeTodos, incompleteTodos[index]]
-
-    newincompletetodos.splice(index, 1)
-
-    setIncompleteTodos(newincompletetodos)
-    setCompleteTodos(newcompletetodos)
-
-
-  }
-
-
-  //戻すボタンが押したときに呼び出される関数
-  //completeTodosにボタンを押されたtodoを追加、incompleteTodosからボタンを押されたtodoを削除する
-  const onClickBackCompleteTodo = (index) => {
-
-    const newincompletetodos = [...incompleteTodos, completeTodos[index]]
-    const newcompletetodos = [...completeTodos]
-
-    newcompletetodos.splice(index, 1)
-
-    setIncompleteTodos(newincompletetodos)
-    setCompleteTodos(newcompletetodos)
-  }
-
-  const isMaxTodoLimit = incompleteTodos.length >= 5;
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='todo-app'>
-        <h1 className='main-title'>Todoアプリ</h1>
-        <InputTodo todoText={todoText} onChangeTodoText={onChangeTodoText} onCkickAddIncompleteTodo={onCkickAddIncompleteTodo} isMaxTodoLimit={isMaxTodoLimit} />
-        {/* 未完了のタスクの数の上限5個に設定 */}
-        {isMaxTodoLimit && <h2 style={{textAlign: 'center',color: 'white'}}>追加できる未完了のタスクは5個までです！</h2>}
-        <br />
-        <IncompleteTodo incompleteTodos={incompleteTodos} onClickCompleteIncompleteTodo={onClickCompleteIncompleteTodo} onClickDeleteIncompleteTodo={onClickDeleteIncompleteTodo} />
-        <CompleteTodo completeTodos={completeTodos} onClickBackCompleteTodo={onClickBackCompleteTodo} />
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
 
+export default App
--- src/conponent/InputTodo.jsx
+++
@@ -1,8 +0,0 @@
-export const InputTodo = ({todoText, onChangeTodoText, onCkickAddIncompleteTodo, isMaxTodoLimit}) => {
-    return (
-        <div className='input-area'>
-            <input disabled={isMaxTodoLimit} type="text" placeholder='TODO入力' value={todoText} onChange={onChangeTodoText} />
-            <button disabled={isMaxTodoLimit} onClick={onCkickAddIncompleteTodo}>追加</button>
-        </div>
-    )
-}
